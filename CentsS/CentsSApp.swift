48d6532ae767d51f504d141d049dc1a39bf34fbf
leungyinchiew@gmail.com <leungyinchiew@gmail.com>
2025-01-27 21:15:52 +0800

    Initial Commit

---
+++ CentsS/CentsSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentsSApp.swift
+//  CentsS
+//
+//  Created by Leung Yin on 27/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CentsSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
